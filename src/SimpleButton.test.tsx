91c22346f5cce8641b25ba59adc328d5604c9a5c
shusaku009 <shusaku009@gmail.com>
2023-10-22 15:12:43 +0900

    update file

--- src/SimpleButton.test.tsx
+++ src/SimpleButton.test.tsx
@@ -10,3 +10,8 @@ test("ボタンをクリックするとON/OFFの表示が切り替わる", async
   await user.click(simpleButton);
   expect(simpleButton).toHaveTextContent("ON");
 });
+
+test("描画されてすぐはOFFと表示されている", () => {
+  const view = render(<SimpleButton />);
+  expect(view.container).toMatchSnapshot();
+})
